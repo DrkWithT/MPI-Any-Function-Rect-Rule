d948614cca6c3e3fd771e2d96d45e5bdde3e2156
drkwitht <derektan3510@gmail.com>
2024-10-11 17:24:01 -0700

    Initial commit.

---
+++ utility.sh
@@ -0,0 +1,22 @@
+# utility.sh: builds program or counts SLOC for project
+
+if [[ $# -ne 1 ]]; then
+    echo "usage: ./utility.sh [help | sloc | deb | rel | test]"
+    exit 1
+fi
+
+choice="$1"
+
+if [[ "$choice" = "sloc" ]]; then
+    wc -l ./src/**/*.cpp ./src/Include/**/*.hpp
+elif [[ "$choice" = "deb" ]]; then
+    cmake --fresh -S . -B Build -DDBG_BUILD:BOOL=1 && cmake --build Build;
+    cp ./Build/compile_commands.json .
+elif [[ "$choice" = "rel" ]]; then
+    cmake --fresh -S . -B Build -DDBG_BUILD:BOOL=0 && cmake --build Build;
+    cp ./Build/compile_commands.json .
+elif [[ "$choice" = "test" ]]; then
+    ctest --test-dir Build -V --timeout 3
+else
+    echo "usage: ./utility.sh [help | sloc | deb | rel | test]"
+fi
