58a19fe14eefcb9a0ca693dabdcea6086e9f328a
drkwitht <derektan3510@gmail.com>
2024-10-11 17:25:01 -0700

    Updated CMake files.

---
+++ src/Main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief Implements MPI C++ program that calculates Rectangle Rule approximation of user entered variable function types, e.g `2x^2 + x - 1`.
+ * @version 0.1.0
+ * @date 2024-10-09
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <iostream>
+// #include "Frontend/Token.hpp"
+
+int main() {
+    std::cout << "Hello Test!\n";
+}
